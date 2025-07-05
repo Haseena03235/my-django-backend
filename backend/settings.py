f98fd20676c75a0814598c6b81ba5d4f50566e3b
Haseena <bashaveerji118@gmail.com>
2025-07-05 11:22:20 +0530

    Save changes before rebase

--- backend/settings.py
+++ backend/settings.py
@@ -15,6 +15,7 @@ import os
 from django.conf import settings
 from django.templatetags.static import static
 import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
